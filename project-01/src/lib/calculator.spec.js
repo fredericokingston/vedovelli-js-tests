11065fed7ffff966e80ea28eb5fb541d4d12fc4f
fredericokingston <fredericovcoelho@gmail.com>
2022-05-01 20:31:39 -0300

    feat: upgrade and add more tests to sum method

--- project-01/src/lib/calculator.spec.js
+++ project-01/src/lib/calculator.spec.js
@@ -3,3 +3,26 @@ const { sum } = require("./calculator");
 it('should be able to sum 2 and 2 and the result must be 4', () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+it('be able to sum 2 and 2 even if one of then is a string and the result must be 4', () => {
+  expect(sum("2", "2")).toBe(4);
+});
+
+it('should throw an error if if what is provided to be sum is not a number', () => {
+  expect(() => {
+    sum("", "2");
+  }).toThrowError();
+
+  expect(() => {
+    sum([2, 2]);
+  }).toThrowError();
+
+  expect(() => {
+    sum({});
+  }).toThrowError();
+
+  expect(() => {
+    sum();
+  }).toThrowError();
+});
+
